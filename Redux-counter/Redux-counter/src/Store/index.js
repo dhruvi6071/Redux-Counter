77df4862f8fcbb97694e70c1d46655035d9add04
dhruvi6071 <dhruvi6071@gmail.com>
2024-05-26 14:00:24 +0530

    Atteching-payloads

--- Redux-counter/Redux-counter/src/Store/index.js
+++ Redux-counter/Redux-counter/src/Store/index.js
@@ -7,6 +7,13 @@ const counterReducer = (state = { counter: 0 }, action) => {
     };
   }
 
+  if (action.type === 'increase') {
+    return {
+      //here we can't increment by some number like 5 or 10. therefore it is a better way for us to use action.amount/
+      counter: state.counter + action.amount,
+    };
+  }
+
   if (action.type === 'decrement') {
     return {
       counter: state.counter - 1,
